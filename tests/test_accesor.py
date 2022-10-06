cfe757bc693ef2e8a7ce1a1ea5e3da56d479e88f
Chris Havlin <chris.havlin@gmail.com>
2022-10-06 12:19:55 -0500

    add a pytest fixture for some of the xr ds

--- tests/test_accesor.py
+++ tests/test_accesor.py
@@ -6,6 +6,25 @@ import yt_xarray  # noqa: F401
 from yt_xarray._utilities import construct_minimal_ds
 
 
+@pytest.fixture()
+def ds_xr():
+    # a base xarray ds to be used in various places.
+    tfield = "a_new_field"
+    n_x = 3
+    n_y = 4
+    n_z = 5
+    ds = construct_minimal_ds(
+        field_name=tfield,
+        n_fields=3,
+        n_x=n_x,
+        n_y=n_y,
+        n_z=n_z,
+        z_name="depth",
+        coord_order=["z", "y", "x"],
+    )
+    return ds
+
+
 def test_accessor():
 
     tfield = "a_new_field"
@@ -71,33 +90,23 @@ def test_bbox():
     # the test dataset.
 
 
-def test_load_uniform_grid():
+def test_load_uniform_grid(ds_xr):
 
-    tfield = "a_new_field"
-    n_x = 3
-    n_y = 4
-    n_z = 5
-    ds = construct_minimal_ds(
-        field_name=tfield,
-        n_fields=3,
-        n_x=n_x,
-        n_y=n_y,
-        n_z=n_z,
-        z_name="depth",
-        coord_order=["z", "y", "x"],
-    )
-
-    flds = [tfield + "_0", tfield + "_1"]
-    ds_yt = ds.yt.load_uniform_grid(flds)
+    flds = ["a_new_field_0", "a_new_field_1"]
+    ds_yt = ds_xr.yt.load_uniform_grid(flds)
     assert ds_yt.coordinates.name == "internal_geographic"
     expected_field_list = [("stream", f) for f in flds]
     assert all([f in expected_field_list] for f in ds_yt.field_list)
 
-    ds_yt = ds.yt.load_uniform_grid()  # should generate a ds with all fields
-    flds = [tfield + "_0", tfield + "_1", tfield + "_2"]
+    ds_yt = ds_xr.yt.load_uniform_grid()  # should generate a ds with all fields
+    flds = flds + [
+        "a_new_field_2",
+    ]
     expected_field_list = [("stream", f) for f in flds]
     assert all([f in expected_field_list] for f in ds_yt.field_list)
 
+    tfield = "nice_field"
+    n_x, n_y, n_z = (7, 5, 17)
     ds = construct_minimal_ds(
         field_name=tfield,
         n_fields=3,
@@ -122,6 +131,9 @@ def test_load_uniform_grid():
         y_name="y",
         coord_order=["z", "y", "x"],
     )
+    flds = [
+        tfield + "_0",
+    ]
     ds_yt = ds.yt.load_uniform_grid(flds, length_unit="km")
     assert ds_yt.coordinates.name == "cartesian"
     assert all([f in expected_field_list] for f in ds_yt.field_list)
@@ -130,46 +142,24 @@ def test_load_uniform_grid():
 @pytest.mark.skipif(
     yt.__version__.startswith("4.1") is False, reason="requires yt>=4.1.0"
 )
-def test_load_grid_from_callable():
-    tfield = "a_new_field"
-    n_x = 3
-    n_y = 4
-    n_z = 5
-    ds_xr = construct_minimal_ds(
-        field_name=tfield,
-        n_fields=3,
-        n_x=n_x,
-        n_y=n_y,
-        n_z=n_z,
-        z_name="depth",
-        coord_order=["z", "y", "x"],
-    )
-
-    flds = [tfield + "_0", tfield + "_1"]
-
+def test_load_grid_from_callable(ds_xr):
     ds = ds_xr.yt.load_grid_from_callable()
+    flds = list(ds_xr.data_vars)
     for fld in flds:
         assert ("stream", fld) in ds.field_list
 
     f = ds.all_data()[flds[0]]
-    assert len(f) == n_x * n_y * n_z
+    assert len(f) == ds_xr.data_vars[flds[0]].size
 
 
-def test_yt_ds_attr():
-    tfield = "a_new_field"
-    n_x = 3
-    n_y = 4
-    n_z = 5
-    ds_xr = construct_minimal_ds(
-        field_name=tfield,
-        n_fields=3,
-        n_x=n_x,
-        n_y=n_y,
-        n_z=n_z,
-        z_name="depth",
-        coord_order=["z", "y", "x"],
-    )
-
-    ds = ds_xr.yt.ds()  # alias to load_grid_from_callable but good to check
-    for fld in list(ds_xr.data_vars):
+@pytest.mark.skipif(
+    yt.__version__.startswith("4.1") is False, reason="requires yt>=4.1.0"
+)
+def test_yt_ds_attr(ds_xr):
+    ds = ds_xr.yt.ds()
+    flds = list(ds_xr.data_vars)
+    for fld in flds:
         assert ("stream", fld) in ds.field_list
+
+    f = ds.all_data()[flds[0]]
+    assert len(f) == ds_xr.data_vars[flds[0]].size
