831c04fd265bae4df95a2f0d094956086c53b52d
chrishavlin <chris.havlin@gmail.com>
2023-01-20 16:20:43 -0600

    update docs to reflect load_grid rename

--- yt_xarray/logging.py
+++ yt_xarray/logging.py
@@ -3,7 +3,7 @@ import logging
 ytxr_log = logging.getLogger("yt_xarray")
 ytxr_log.setLevel(logging.INFO)
 
-_formatter = logging.Formatter("%(name)s %(asctime)s: %(levelname)s: %(message)s")
+_formatter = logging.Formatter("%(name)s : [%(levelname)s ] %(asctime)s:  %(message)s")
 
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(_formatter)
